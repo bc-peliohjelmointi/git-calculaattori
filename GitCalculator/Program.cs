7ddb4d8535869da4ea7ee144dd3eb2290b8cdfa2
manee-bc <eero.manner@bc.fi>
2022-01-27 12:28:11 +0200

    Tehtiin yksinkertainen x+y laskin.

---
+++ GitCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.WriteLine("Tervettuloa Calculaattoriin! Lasketaan yksinkertainen yhteenlasku.");
+
+int firstNumber, secondNumber;
+
+Console.WriteLine("Lasketaan x + y. Anna x:");
+firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Anna y:");
+secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int summa = firstNumber + secondNumber;
+
+Console.WriteLine("Lasketaan...");
+Console.WriteLine(firstNumber + " + " + secondNumber + " = " + summa);
