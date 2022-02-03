2836692a389edbe2c24c497cf0195e4fbc831a4c
manee-bc <eero.manner@bc.fi>
2022-02-03 12:56:02 +0200

    Lisättiin erotus tulostus.

--- GitCalculator/Program.cs
+++ GitCalculator/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 
-Console.WriteLine("Tervettuloa Calculaattoriin! Lasketaan yksinkertainen yhteenlasku.");
+Console.WriteLine("Tervettuloa Calculaattoriin! Lasketaan yksinkertainen yhteenlasku ja vähennyslasku.");
 
 int firstNumber, secondNumber;
 
-Console.WriteLine("Lasketaan x + y. Anna x:");
+Console.WriteLine("Anna x:");
 firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Anna y:");
 secondNumber = Convert.ToInt32(Console.ReadLine());
 
 int summa = firstNumber + secondNumber;
+int erotus = firstNumber - secondNumber;
 
-Console.WriteLine("Lasketaan...");
+Console.WriteLine("Lasketaan x + y ja x - y...");
 Console.WriteLine(firstNumber + " + " + secondNumber + " = " + summa);
+Console.WriteLine(firstNumber + " - " + secondNumber + " = " + erotus);
